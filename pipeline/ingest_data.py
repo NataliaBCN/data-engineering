c9f56ccf66b14e7107b5129d6ed67adc67c67b04
NataliaBCN <lassonda@gmail.com>
2026-01-17 22:33:31 +0000

    last part of docker module

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -29,25 +30,20 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-
-def run():
-
-    pg_user='root'
-    pg_pass='root'
-    pg_host='localhost'  
-    pg_port=5432
-    pg_db='ny_taxi'
-    
-    year=2021
-    month=1    
-    
-    target_table='yellow_taxi_data'
-    
-    chunksize=100000
-    
-    prefix='https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-    url=f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
-    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+@click.command()
+@click.option('--user', default='root', help='PostgreSQL user')
+@click.option('--password', default='root', help='PostgreSQL password')
+@click.option('--host', default='localhost', help='PostgreSQL host')
+@click.option('--port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--table', default='yellow_taxi_data', help='Target table name')
+@click.option('--year', type=int, help='Year of the data')
+@click.option('--month', type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
+def ingest_data(user, password, host, port, db, table, year, month, chunksize):
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
     df_iter = pd.read_csv(
         url,
@@ -57,22 +53,22 @@ def run():
         chunksize=chunksize,
     )
 
-    first=True
+    first = True
     for df_chunk in tqdm(df_iter):
         if first:
             df_chunk.head(0).to_sql(
-                name=target_table,
+                name=table,
                 con=engine,
                 if_exists='replace'
             )
-            first=False
+            first = False
 
         df_chunk.to_sql(
-            name=target_table,
+            name=table,
             con=engine,
             if_exists='append'
         )
-    
+
 if __name__ == '__main__':
-    run()
+    ingest_data()
     
